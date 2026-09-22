447b8acaf6e9b3a7cdbe7bd6894d94ee39cd7b58
Sihina Lahiru <149120815+Sihnalahiru@users.noreply.github.com>
2026-09-22 21:07:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,156 +1,185 @@
-const CACHE_NAME = "irodori-master-v3";
+const CACHE_NAME = "irodori-master-v4";
+
 
 const APP_SHELL = [
+
   "./",
+
   "./index.html",
 
-  /* CSS */
   "./css/style.css",
 
-  /* JavaScript */
   "./js/app.js",
+
   "./js/progress.js",
 
-  /* PWA */
   "./manifest.json",
 
-  /* Master Data */
   "./data/books.json",
+
   "./data/lessons.json",
 
-  /* Can-do Data */
   "./data/canDos-starter.json",
+
   "./data/canDos-e1.json",
+
   "./data/canDos-e2.json",
+
   "./data/canDos-pi.json"
+
 ];
+
 
 
 /* =========================================
    INSTALL
    ========================================= */
 
-self.addEventListener("install", (event) => {
+self.addEventListener(
+  "install",
+  (event) => {
 
-  event.waitUntil(
+    event.waitUntil(
 
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => {
+      caches
+        .open(CACHE_NAME)
+        .then(
+          (cache) => {
 
-        return cache.addAll(APP_SHELL);
+            return cache.addAll(
+              APP_SHELL
+            );
 
-      })
+          }
+        )
 
-  );
+    );
 
-  self.skipWaiting();
 
-});
+    self.skipWaiting();
+
+  }
+);
+
 
 
 /* =========================================
    ACTIVATE
    ========================================= */
 
-self.addEventListener("activate", (event) => {
+self.addEventListener(
+  "activate",
+  (event) => {
 
-  event.waitUntil(
+    event.waitUntil(
 
-    caches
-      .keys()
-      .then((cacheNames) => {
+      caches
+        .keys()
+        .then(
+          (cacheNames) => {
 
-        return Promise.all(
+            return Promise.all(
 
-          cacheNames
-            .filter(
-              (name) => name !== CACHE_NAME
-            )
-            .map(
-              (name) => caches.delete(name)
-            )
+              cacheNames
 
-        );
+                .filter(
+                  (name) =>
+                    name !== CACHE_NAME
+                )
 
-      })
+                .map(
+                  (name) =>
+                    caches.delete(name)
+                )
 
-  );
+            );
 
-  self.clients.claim();
+          }
+        )
 
-});
+    );
+
+
+    self.clients.claim();
+
+  }
+);
+
 
 
 /* =========================================
    FETCH
    ========================================= */
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener(
+  "fetch",
+  (event) => {
 
-  event.respondWith(
+    event.respondWith(
 
-    caches
-      .match(event.request)
-      .then((cachedResponse) => {
+      caches
+        .match(event.request)
+        .then(
+          (cachedResponse) => {
 
-        /* -----------------------------
-           USE CACHE IF AVAILABLE
-           ----------------------------- */
+            if (cachedResponse) {
 
-        if (cachedResponse) {
-
-          return cachedResponse;
-
-        }
-
-
-        /* -----------------------------
-           OTHERWISE USE NETWORK
-           ----------------------------- */
-
-        return fetch(event.request)
-
-          .then((networkResponse) => {
-
-            /*
-             * Only cache successful
-             * same-origin responses.
-             */
-
-            if (
-              !networkResponse ||
-              networkResponse.status !== 200 ||
-              networkResponse.type === "opaque"
-            ) {
-
-              return networkResponse;
+              return cachedResponse;
 
             }
 
 
-            const responseClone =
-              networkResponse.clone();
+            return fetch(
+              event.request
+            )
+
+              .then(
+                (networkResponse) => {
+
+                  if (
+
+                    !networkResponse ||
+
+                    networkResponse.status !==
+                      200 ||
+
+                    networkResponse.type ===
+                      "opaque"
+
+                  ) {
+
+                    return networkResponse;
+
+                  }
 
 
-            caches
-              .open(CACHE_NAME)
-              .then((cache) => {
-
-                cache.put(
-                  event.request,
-                  responseClone
-                );
-
-              });
+                  const responseClone =
+                    networkResponse.clone();
 
 
-            return networkResponse;
+                  caches
+                    .open(CACHE_NAME)
+                    .then(
+                      (cache) => {
 
-          });
+                        cache.put(
+                          event.request,
+                          responseClone
+                        );
 
-      })
+                      }
+                    );
 
-  );
 
-});
+                  return networkResponse;
+
+                }
+              );
+
+          }
+        )
+
+    );
+
+  }
+);
